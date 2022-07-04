b0fc269146632a97fa172f70c18fdd672d10629b
Uladzimir <vl.barbikov@gmail.com>
2022-07-04 16:33:49 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        n = n * -1;
+        n = n.toString();
+        return n = Number(n.split("").reverse().join(""));
+    }
+    else {
+    n = n.toString();
+    return n = Number(n.split("").reverse().join(""));
+    }
 }
